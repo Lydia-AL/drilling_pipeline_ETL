81240ddb62e1e9b8073f56db983275d18c18f8ba
Lydia-AL <lydia.aitlamara@gmail.com>
2026-03-13 22:31:32 +0100

    sorting and iterating through raw JSONs

--- main.py
+++ main.py
@@ -6,13 +6,7 @@ raw_dir = "data/raw"
 processed_dir = "data/processed"
 os.makedirs(processed_dir, exist_ok=True)
 
-files = [f for f in os.listdir(raw_dir) if f.endswith(".json")]
-
-for filename in files:
-    in_path = os.path.join(raw_dir, filename)
-    with open(in_path, "r") as f:
-        data = json.load(f)
-    processed = process_machine(data)
+files = sorted([f for f in os.listdir(raw_dir) if f.endswith(".json")])
 
 for filename in files:
     in_path = os.path.join(raw_dir, filename)
